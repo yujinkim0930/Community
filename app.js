cbe5c26b704e55ae3feed12ade79214046e915ac
kim yujin <yujin3009@naver.com>
2024-02-07 22:50:23 +0900

    feat: 게시글 작성 api

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import UsersRouter from './routes/users.router.js';
 import PostsRouter from './routes/posts.router.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = 3018;
 
-app.use([UsersRouter, PostsRouter]);
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api', [UsersRouter, PostsRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
--- routes/posts.router.js
+++ routes/posts.router.js
@@ -1,5 +1,19 @@
 import express from 'express';
+import { prisma } from '../model/index.js';
+import { PrismaClient } from '@prisma/client';
 
 const router = express.Router();
-
+router.post('/posts', async (req, res) => {
+  const { title, category, content } = req.body;
+  const { id } = req.user;
+  const post = await prisma.posts.create({
+    data: {
+      user_Id: +id,
+      title,
+      category,
+      content,
+    },
+  });
+  return res.status(201).json({ message: '게시글이 작성되었습니다.' });
+});
 export default router;
