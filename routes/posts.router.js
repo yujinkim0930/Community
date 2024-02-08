ed47c059cf591a62210febfc202a3c97e388deaf
HA0N1 <intor48@gmail.com>
2024-02-08 18:51:02 +0900

    feat: try catch

--- routes/posts.router.js
+++ routes/posts.router.js
@@ -1,20 +1,40 @@
-import express from 'express';
+import express, { json } from 'express';
 import prisma from '../models/index.js';
 import authMiddleware from '../middlewares/auth.Middleware.js';
 import { PrismaClient } from '@prisma/client';
 
 const router = express.Router();
 router.post('/posts', authMiddleware, async (req, res) => {
-  const { title, category, content } = req.body;
-  const user = res.locals.user;
-  const post = await prisma.posts.create({
-    data: {
-      user_Id: user.id,
-      title,
-      category,
-      content,
-    },
-  });
-  return res.status(201).json({ message: '게시글이 작성되었습니다.' });
+  try {
+    const { title, category, content } = req.body;
+    const user = res.locals.user;
+    if (!title)
+      return res
+        .status(400)
+        .json({ success: false, message: '게시글 제목은 필수 값입니다.' });
+    if (!category)
+      return res
+        .status(400)
+        .json({ success: false, message: '카테고리는 필수 값입니다.' });
+    if (!content)
+      return res
+        .status(400)
+        .json({ success: false, message: '게시글 내용은 필수값입니다.' });
+    if (!['INFO', 'CLUB', 'LOST'].includes(category))
+      return res
+        .status(400)
+        .json({ success: false, message: '카테고리가 올바르지 않습니다.' });
+    await prisma.posts.create({
+      data: {
+        user_Id: user.id,
+        title,
+        category,
+        content,
+      },
+    });
+    return res.status(201).json({ message: '게시글이 작성되었습니다.' });
+  } catch (error) {
+    return res.status(400).json({ success: false, message: error.message });
+  }
 });
 export default router;
